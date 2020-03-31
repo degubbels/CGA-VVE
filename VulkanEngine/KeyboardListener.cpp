91b4504ac28ed656c85a86c62ba5cf866057e769
degu <dev@degu.info>
2020-03-31 21:17:12 +0000

    move cat

--- VulkanEngine/KeyboardListener.cpp
+++ VulkanEngine/KeyboardListener.cpp
@@ -3,13 +3,49 @@
 
 namespace ve {
 
+	// Movement speed
+	float speed = 1.2f;
+	float rotSpeed = 2.0;
+
+
 	/**
 	 *	Contiuously take screenshots
 	 */
 	bool KeyboardListener::onKeyboard(veEvent event) {
-		printf("key input\n");
+
+		VESceneNode* catP = getSceneManagerPointer()->getSceneNode("catP");
+		VESceneNode* cat = getSceneManagerPointer()->getSceneNode("cat");
+
+		glm::vec4 translate = glm::vec4(0.0, 0.0, 0.0, 1.0);	//total translation
+		glm::vec4 rot4 = glm::vec4(1.0);
+		float angle = 0.0;
+
+		switch (event.idata1) {
+		case GLFW_KEY_W:
+			translate = cat->getTransform() * glm::vec4(0.0, 0.0, 1.0, 1.0); //forward
+			translate.y = 0.0f;
+			break;
+		case GLFW_KEY_A:
+			angle = rotSpeed * (float)event.dt * -1.0f;
+			rot4 = glm::vec4(0.0, 1.0, 0.0, 1.0);
+			break;
+		case GLFW_KEY_D:
+			angle = rotSpeed * (float)event.dt * 1.0f;
+			rot4 = glm::vec4(0.0, 1.0, 0.0, 1.0);
+			break;
+		default:
+			break;
+		}
+
+		glm::vec3 trans = speed * glm::vec3(translate.x, translate.y, translate.z);
+		catP->multiplyTransform(glm::translate( glm::mat4(1.0f), (float)event.dt * trans) );
+
+		glm::vec3  rot3 = glm::vec3(rot4.x, rot4.y, rot4.z);
+		glm::mat4  rotate = glm::rotate(glm::mat4(1.0), angle, rot3);
+		cat->multiplyTransform(rotate);
+
+		//printf("%d, %d, %d\n", trans.x, trans.y, trans.z);
+
 		return false;
-
-
 	}
 }
--- VulkanEngine/main.cpp
+++ VulkanEngine/main.cpp
@@ -179,15 +179,16 @@ namespace ve {
 			eParent->addChild(e1);
 
 			// Load cat model
-			VESceneNode* cat;
-			VECHECKPOINTER(cat= getSceneManagerPointer()->loadModel("sphere", "media/models/test/cat", "12221_Cat_v1_l3.obj", aiProcess_FlipWindingOrder | aiProcess_FlipUVs, pScene));
-			cat->setTransform(glm::scale(glm::mat4(0.1f), glm::vec3(0.02f, 0.02f, 0.02f)));
+			VESceneNode* cat, *catParent;
+			catParent = getSceneManagerPointer()->createSceneNode("catP", pScene, glm::mat4(1.0));
+			VECHECKPOINTER(cat= getSceneManagerPointer()->loadModel("cat", "media/models/test/cat", "12221_Cat_v1_l3.obj", aiProcess_FlipWindingOrder | aiProcess_FlipUVs, catParent));
+			catParent->setTransform(glm::scale(glm::mat4(0.1f), glm::vec3(0.02f, 0.02f, 0.02f)));
 
 			// Rotate 90 deg around x-axis
 			cat->multiplyTransform(glm::rotate( glm::mat4(1.0), -glm::half_pi<float>(), glm::vec3(1.0, 0.0, 0.0) ));
 
 			// Move up to ground level
-			cat->multiplyTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, CAT_Y_OFFSET, 0.0f)));
+			catParent->multiplyTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, CAT_Y_OFFSET, 0.0f)));
 
 
 
